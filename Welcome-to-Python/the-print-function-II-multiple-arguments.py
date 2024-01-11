b71faf4d2175dd41486270a1b4734f406699d49e
91898 <chandansatyendraprasad@gmail.com>
2024-01-11 16:39:32 +0530

    first commit

---
+++ Welcome-to-Python/the-print-function-II-multiple-arguments.py
@@ -0,0 +1,8 @@
+print("A", "B")
+print("C", "D", "E", "F")
+print("G", "H", "I", "J", "K", "L", "M", "N")
+print(5 + 3, 2 - 8)
+print(5 + 3, 2 - 8, 3 + 1)
+print(5 - 3, 2 + 8)
+
+print("Hello", 10, 3.14)
---
+++ Welcome-to-Python/the-print-function-III-parameters-and-arguments.py
@@ -0,0 +1,21 @@
+print("ABC", "DEF", "XYZ")
+print("ABC", "DEF", "XYZ", sep="!")
+print("ABC", "DEF", "XYZ", sep="--*--")
+print("ABC", "DEF", "XYZ", sep=" ")
+
+print("ABC", "DEF", "XYZ", "!")
+
+print("Hello", "Goodbye", end="!&*")
+print("Hello")
+
+print("A", "B", "C", sep="**", end="#")
+print("A", "B", "C", end="#", sep="**")
+a=17
+b=8
+d=a/b
+print("SUM =",a+b)
+print("DIFFERENCE =",a-b)
+print("MUL =",a*b)
+print("DIV =",d)
+print(type(d))
+# to know the data type
